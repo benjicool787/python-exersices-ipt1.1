70a0b91355ed7ab082473bacaa02759c28f26a91
Benjamin Toros Pigeon <benjamin@BenBun.localdomain>
2026-01-20 11:42:12 +0100

    dec-to-roman.py

---
+++ dec-to-roman.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+
+wert = sys.argv[1]
+
+if wert == "1":
+    print("I")
+elif wert == "2":
+    print("II")
+elif wert == "3":
+    print("III")
+elif wert == "4":
+    print("IV")
+elif wert == "5":
+    print("V")
+elif wert == "6":
+    print("VI")
+elif wert == "7":
+    print("VII")
+elif wert == "8":
+    print("VIII")
+elif wert == "9":
+    print("IX")
+else:
+    print("Die spinnen, die Römer.")
+
